387bea79381d2d8d1bf578989ee89531577ac643
Jackmrzhou <594780735@qq.com>
2019-03-03 17:29:10 +0800

    modify make.bat

--- make.bat
+++ make.bat
@@ -4,3 +4,5 @@ set GOARCH=amd64
 go build -a -installsuffix cgo -o gc_ai .
 go_swagger generate spec -o swaggerui/swagger.json
 del gc_ai.tar
+docker build --no-cache -t gc-ai .
+docker save gc-ai -o gc_ai.tar
